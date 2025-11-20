ab76854e0836f4dcc653f0c261c00abaf5eef348
Nurish <zhandarbekov.t.e@gmail.com>
2025-11-20 13:51:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 def add(a, b):
+    """Екі санды қосу"""
     return a + b
 
+def multiply(a, b):
+    """Екі санды көбейту"""
+    return a * b
+
 if __name__ == "__main__":
-    print(add(2, 3))
+    print("2 + 3 =", add(2, 3))
+    print("2 * 3 =", multiply(2, 3))
